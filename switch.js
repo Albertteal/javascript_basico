5db89c591099a37442323a1782df83972fb1ad52
Albert Nuñez <albertteal@gmai.com>
2023-10-02 10:46:08 -0600

    Subiendo el archivo de switch

---
+++ switch.js
@@ -0,0 +1,13 @@
+var numero =3;
+
+switch (numero){
+    case 1:
+        console.log("Soy uno");
+        break;
+    case 2:
+        console.log("Soy un 2");
+        break;
+    default:
+        console.log("Soy nada");
+}
+
